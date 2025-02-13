4760d5bf414993139fa5fa371130310e1836115b
PedroVidalDev <pedrohvidals@gmail.com>
2025-02-12 21:45:37 -0300

    feat: create initial routes for scrapper

--- src/routes/SigaRouter.ts
+++ src/routes/SigaRouter.ts
@@ -1,9 +1,14 @@
-import { Router } from "express";
+import { Request, Router } from "express";
+import { SigaController } from "../controllers/SigaController";
+
+const controller = new SigaController();
 
 const router = Router();
 
-router.use("/", () => {
+router.get("/", () => {
     console.log("siga")
 })
+
+router.get("/absence", (req: Request, res: Response) => controller.getAbsencesInfo(req, res));
 
 export default router;

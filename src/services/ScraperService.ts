38b11626bd24bf356e99da7017176fc1cd2e3cc3
PedroVidalDev <pedrohvidals@gmail.com>
2025-02-13 19:42:22 -0300

    feat: get the absences info from siga using puppeteer

--- src/services/ScraperService.ts
+++ src/services/ScraperService.ts
@@ -2,6 +2,7 @@ import puppeteer, { Browser, Page } from "puppeteer";
 
 import { LoginDTO } from "../dtos/Auth/LoginDTO";
 import { ScrapUtils } from "../utils/ScrapUtils";
+import { AbsencesDTO } from "../dtos/Absences/AbsencesDTO";
 
 export class ScraperService {
     private url: string = "https://siga.cps.sp.gov.br/aluno/login.aspx";
@@ -18,7 +19,7 @@ export class ScraperService {
         this.page = await browser.newPage();
     }
 
-    public async login(loginDto: LoginDTO) {
+    public async login(loginDto: LoginDTO): Promise<boolean> {
         await this.page.goto(this.url);
 
         await ScrapUtils.typeInput(this.page, "#vSIS_USUARIOID", loginDto.username);
@@ -30,9 +31,35 @@ export class ScraperService {
         const isLoginSuccessfull = await this.page.evaluate(() => {
             return document.querySelector("#TABLE5") !== null;
         })
+
+        return isLoginSuccessfull;
     }
 
     public async getAbsencesInfo(loginDto: LoginDTO) {
-        this.login(loginDto);
+        if(await this.login(loginDto)) {
+            await this.page.click("#ygtvlabelel11Span");
+            await this.page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+            const tableLines = await this.page.$$(".GridClearOdd");
+
+            const absencesList: AbsencesDTO[] = [];
+
+            for (const line of tableLines) {
+                const textContents: string[] = [];
+
+                const items = await line.$$("td");
+
+                for (const element of items) {
+                    const text = await this.page.evaluate(el => el.textContent?.trim(), element);
+                    if(text) {
+                        textContents.push(text);
+                    }
+                };
+
+                absencesList.push(new AbsencesDTO(Number(textContents[2]), Number(textContents[3]), textContents[1], "teste"));
+            };
+
+            return absencesList;
+        }
     }
 }

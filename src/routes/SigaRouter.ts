026607bcb7b3a2a2cf223593369316ab2ba4d776
PedroVidalDev <pedrohvidals@gmail.com>
2025-02-13 15:23:20 -0300

    refactor: alter route endpoint

--- src/routes/SigaRouter.ts
+++ src/routes/SigaRouter.ts
@@ -9,6 +9,6 @@ router.get("/", () => {
     console.log("siga")
 })
 
-router.get("/absence", (req: Request, res: Response) => controller.getAbsencesInfo(req, res));
+router.get("/absences", (req: Request, res: Response) => controller.getAbsencesInfo(req, res));
 
 export default router;

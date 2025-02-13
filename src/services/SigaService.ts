036a18f9dc3c2d128b7a626b26e72edcc57dcb11
PedroVidalDev <pedrohvidals@gmail.com>
2025-02-13 09:24:51 -0300

    refactor: create a service for scraper

--- src/services/SigaService.ts
+++ src/services/SigaService.ts
@@ -1,38 +1,14 @@
-import puppeteer, { Browser, Page } from "puppeteer";
-
 import { LoginDTO } from "../dtos/Auth/LoginDTO";
+import { ScraperService } from "./ScraperService";
 
 export class SigaService {
-    private url: string = "https://siga.cps.sp.gov.br/aluno/login.aspx";
-    private browser!: Browser;
-    private page!: Page;
+    private scraperService: ScraperService;
 
-    public async initPage() {
-        const browser = await puppeteer.launch({ headless: false });
-        this.browser = browser;
-        this.page = await browser.newPage();
-    }
-
-    public async login(loginDto: LoginDTO) {
-        await this.page.goto(this.url);
-        await this.page.type("#vSIS_USUARIOID", loginDto.username);
-        await this.page.type("#vSIS_USUARIOSENHA", loginDto.password);
-        await this.page.click("button[name='BTCONFIRMA']");
-        await this.page.waitForNavigation({ waitUntil: 'networkidle0' });
-        const isLoginSuccessfull = await this.page.evaluate(() => {
-            return document.querySelector("#TABLE5") !== null;
-        })
-
-        if(isLoginSuccessfull) {
-            console.log("LOGOUU")
-        } else {
-            console.log("NAO LOGOU INFENROOOOO")
-        }
-
-        await this.browser.close();
+    constructor() {
+        this.scraperService = new ScraperService();
     }
 
     public async getAbsencesInfo(loginDto: LoginDTO) {
-        this.login(loginDto);
+        this.scraperService.getAbsencesInfo(loginDto);
     }
 }

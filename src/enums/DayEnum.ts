7b92cbd953260f7c71c995e035233452f9ae9036
PedroVidalDev <pedrohvidals@gmail.com>
2025-02-14 11:18:07 -0300

    feat: create enum for days

---
+++ src/enums/DayEnum.ts
@@ -0,0 +1,8 @@
+export enum DayEnum {
+    MONDAY = "monday",
+    TUESDAY = "tuesday",
+    WEDNESDAY = "wednesday",
+    THURSDAY = "thursday",
+    FRIDAY = "friday",
+    SATURDAY = "saturday"
+}

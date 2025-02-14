e3fffb93c48a5298762e6e446acaaaa5fc948c84
PedroVidalDev <pedrohvidals@gmail.com>
2025-02-14 11:17:48 -0300

    feat: create routes for get classtime

--- src/controllers/SigaController.ts
+++ src/controllers/SigaController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { LoginDTO } from "../dtos/Auth/LoginDTO";
 import { SigaService } from "../services/SigaService";
+import { DayEnum } from "../enums/DayEnum";
 
 export class SigaController {
     private service: SigaService;
@@ -13,6 +14,14 @@ export class SigaController {
         const { username, password } = req.body;
 
         const data = await this.service.getAbsencesInfo(new LoginDTO(username, password));
+
+        res.status(200).json(data)
+    }
+
+    public async getClasstimes(req: Request, res: Response) {
+        const { username, password } = req.body;
+
+        const data = await this.service.getClasstimeInfo(new LoginDTO(username, password), req.query.day as DayEnum);
 
         res.status(200).json(data)
     }

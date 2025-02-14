a4e4c18dfb0e3a3889340402fdd17d685873da51
PedroVidalDev <pedrohvidals@gmail.com>
2025-02-13 21:51:51 -0300

    refactor: change dto name

--- src/dtos/Absences/AbsenceDTO.ts
+++ src/dtos/Absences/AbsenceDTO.ts
@@ -1,10 +1,10 @@
-export class AbsencesDTO {
+export class AbsenceDTO {
     public presences: number;
     public absences: number;
     public discipline: string;
-    public teacher: string;
+    public teacher?: string;
 
-    constructor(presences: number, absences: number, discipline: string, teacher: string) {
+    constructor(presences: number, absences: number, discipline: string, teacher?: string) {
         this.presences = presences;
         this.absences = absences;
         this.discipline = discipline;

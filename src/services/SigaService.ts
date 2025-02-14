3f63125db995f89abbcd3c58f261156a847728bd
PedroVidalDev <pedrohvidals@gmail.com>
2025-02-14 11:18:36 -0300

    feat: create method in siga service to get the classtime from puppeteer

--- src/services/SigaService.ts
+++ src/services/SigaService.ts
@@ -1,5 +1,7 @@
 import { AbsenceDTO } from "../dtos/Absences/AbsenceDTO";
 import { LoginDTO } from "../dtos/Auth/LoginDTO";
+import { ClasstimeDTO } from "../dtos/Classtime/ClasstimeDTO";
+import { DayEnum } from "../enums/DayEnum";
 import { ScraperService } from "./ScraperService";
 
 export class SigaService {
@@ -13,6 +15,15 @@ export class SigaService {
         const absencesList: AbsenceDTO[] | undefined = await this.scraperService.getAbsencesInfo(loginDto);
         if(absencesList) {
             return absencesList;
+        } else {
+            throw new Error("Erro ao pegar informações.");
+        }
+    }
+
+    public async getClasstimeInfo(loginDto: LoginDTO, day: DayEnum): Promise<ClasstimeDTO[]> {
+        const classtimeList: ClasstimeDTO[] | undefined = await this.scraperService.getClasstimeInfo(loginDto, day);
+        if(classtimeList) {
+            return classtimeList;
         } else {
             throw new Error("Erro ao pegar informações.");
         }

ef8d1b4e8346494ccaba9d70ff250fc2a72bd4bd
PedroVidalDev <pedrohvidals@gmail.com>
2025-02-27 19:15:53 -0300

    feat: create database class with singleton pattern to initialize the db

---
+++ src/config/database/DatabaseConn.ts
@@ -0,0 +1,35 @@
+import { DataSource } from "typeorm";
+import { DisciplineModel } from "../../models/DisciplineModel";
+
+export class DatabaseConn {
+    private static instance: DataSource;
+
+    private constructor(){}
+
+    public static getInstance(): DataSource {
+        if(!DatabaseConn.instance) {
+            DatabaseConn.instance = new DataSource({
+                type: 'postgres',
+                host: process.env.POSTGRES_HOST,
+                port: Number(process.env.POSTGRES_PORT),
+                username: process.env.POSTGRES_USER,
+                password: process.env.POSTGRES_PASSWORD,
+                database: process.env.POSTGRES_DATABASE,
+                synchronize: process.env.NODE_ENV === "prod" ? false : true,
+                logging: true,
+                entities: [DisciplineModel],
+                migrations: [],
+                subscribers: [],
+            });
+
+            DatabaseConn.instance.initialize()
+                .then(() => {
+                    console.log("Connection has been initialized")
+                })
+                .catch((error) => {
+                    console.log("Error during the database initialization: ", error);
+                })
+        }
+        return DatabaseConn.instance;
+    }
+}
